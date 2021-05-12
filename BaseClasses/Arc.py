520254f7d0c94fa049bb3bac9416a44cbdc87b09
aziznal <aziznal.dev@gmail.com>
2021-05-12 05:27:07 +0300

    Implement basic functionality in Arc.py and Street.py to show an arrow pointing to connected node

--- BaseClasses/Arc.py
+++ BaseClasses/Arc.py
@@ -4,7 +4,7 @@ An Arc is a *directional* connection between two Nodes
 
 """
 
-from numpy import arctan
+import numpy as np
 
 
 class Arc:
@@ -14,7 +14,8 @@ class Arc:
 
         self.slope = self.calculate_slope()
 
-        self.angle = self.calculate_angle()
+        self.angle = self.calculate_angle(in_radians=False)
+        self.angle_in_radians = self.calculate_angle(in_radians=True)
 
         self.length = self.calculate_length()
 
@@ -36,10 +37,64 @@ class Arc:
         delta_y = y2 - y1
         delta_x = x2 - x1
 
-        return delta_y / delta_x
+
+        if delta_x == 0:
+            return np.inf
 
 
-    def calculate_angle(self):
+        return -delta_y / delta_x
+
+
+    def get_quadrant_of_node2(self):
+        """
+        Returns quadrant of node2 in reference to node1 based on the unit circle.
+        Note: This takes into account the pygame cartesian coordinate system behavior.
+        """
+        
+        # TODO: check behavior of edge cases: x1 == x2 | y1 == y2
+
+        x1, y1, x2, y2 = self.node1.x, self.node1.y, self.node2.x, self.node2.y
+
+        # Left
+        if x2 < x1:
+            
+            # Top
+            if y2 < y1:
+                return 2
+
+            # Bottom or center
+            else:
+                return 3
+
+        # Right or center
+        else:
+
+            # Top
+            if y2 < y1:
+                return 1
+
+            # Bottom or center
+            else: 
+                return 4
+
+
+
+    def _adjust_for_quadrant(self, angle_in_radians):
+
+        quadrant = self.get_quadrant_of_node2()
+
+        if quadrant == 2:
+            angle_in_radians += 2 * np.pi/2
+
+        elif quadrant == 3:
+            angle_in_radians = -angle_in_radians + 3 * np.pi/2
+
+        elif quadrant == 4:
+            angle_in_radians += 4 * np.pi/2
+
+        return angle_in_radians
+
+    def calculate_angle(self, in_radians=False):
 
         """
         Note: angle is calculated according to positive x-axis
@@ -49,4 +104,16 @@ class Arc:
         #   
         #       Angle = arctan(slope)
 
-        return arctan(self.slope)        
+        angle_in_radians = np.arctan(self.slope)
+
+        angle_in_radians = self._adjust_for_quadrant(angle_in_radians)
+
+
+        if in_radians:
+            
+            return angle_in_radians
+
+        else:
+
+            angle_in_degrees = angle_in_radians * 180 / np.pi
+            return angle_in_degrees
--- Street.py
+++ Street.py
@@ -7,27 +7,57 @@ from PygameSettings import *
 import pygame
 
 
+import numpy as np
+
 
 
 class Street(Arc):
 
     def __init__(self, *args, show_arrows=True):
         super().__init__(*args)
+
         self.show_arrows = show_arrows
 
-    def draw_arrow(self, screen):
-        """
-        arrows will be drawn 
-        """
+        self.arrow_surface = self.create_arrow_surface()
+
+
+    def create_arrow_surface(self):
         
+        # TODO: replace this f**kery with a simple image of an arrow instead
+
+        arrow_surface = pygame.Surface((20, 20), pygame.SRCALPHA)
+        arrow_surface.fill((255, 255, 255))
+
+
+        x, y = self.node2.x, self.node2.y
+
+        norm_x, norm_y = x * 20 / SCREEN_WIDTH, y * 20 / SCREEN_HEIGHT
+        
+        scale = 10
+
+
+        rotation_angle = self.angle
+        rotation_coef = scale * (2**0.5 / 2)
+
+        print(f"Rotation Angle: {rotation_angle}")
+
         points = [
-            (self.node2.x, self.node2.y),
-            (self.node2.x + 10, self.node2.y + 10),
-            (self.node2.x - 10, self.node2.y - 10)
+            
+            (norm_x - rotation_coef, norm_y + rotation_coef),
+            (norm_x, norm_y),
+            (norm_x - rotation_coef, norm_y - rotation_coef)
+
         ]
 
         # An arrow is just a triangle. That's easy enough to draw
-        pygame.draw.polygon(screen, Colors.BLUE, points)
+        pygame.draw.polygon(arrow_surface, Colors.BLUE, points)
+        arrow_surface = pygame.transform.rotate(arrow_surface, rotation_angle)
+
+        return arrow_surface
+
+
+    def draw_arrow(self, screen):
+        screen.blit(self.arrow_surface, (self.node2.x, self.node2.y))
 
 
     def draw(self, screen: pygame.Surface):
