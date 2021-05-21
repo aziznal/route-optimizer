f1152e9491bc8b5df2d053de286fe847809c4d73
aziznal <aziznal.dev@gmail.com>
2021-05-21 21:08:43 +0300

    Draws annotations above nodes displaying their x and y

--- Building.py
+++ Building.py
@@ -5,8 +5,9 @@ from PygameSettings import *
 
 from BaseClasses.Node import Node
 
+import utils
 
-# TODO: Add method to draw node index or coordinates on top of node
+
 
 class Building(Node):
 
@@ -14,9 +15,12 @@ class Building(Node):
         super().__init__(*args, **kwargs)
 
         self.rect = pygame.Rect(self.x, self.y, NODE_RADIUS, NODE_RADIUS)
+        self.text = {"text":f"({self.rect.x}, {self.rect.y})", "font_size":9, "color":Colors.BLACK, "bold":True}
 
     def draw(self, screen: pygame.Surface) -> None:
 
         pygame.draw.ellipse(
             screen, Colors.RED, self.rect
         )
+
+        screen.blit(utils.create_text(**self.text), (self.rect.centerx - 20, self.rect.centery - NODE_RADIUS*2))
