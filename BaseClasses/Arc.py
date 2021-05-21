07ba9af780bb8be1a89038a4d7238537363575ce
aziznal <aziznal.dev@gmail.com>
2021-05-21 20:23:34 +0300

    Cleans up and does some minor refactoring to Arc.py

--- BaseClasses/Arc.py
+++ BaseClasses/Arc.py
@@ -5,9 +5,10 @@ An Arc is a *directional* connection between two Nodes
 """
 
 import numpy as np
-from .Node import Node
 
 from typing import Literal, Union
+
+from .Node import Node
 
 class Arc:
     def __init__(self, node1: Node, node2: Node) -> None:
@@ -21,7 +22,6 @@ class Arc:
 
         self.length = self.calculate_length()
 
-    
     def calculate_length(self) -> float:
         """
         Returns length of Arc connecting the two nodes
@@ -31,10 +31,9 @@ class Arc:
             (self.node1.x - self.node2.x)**2 + (self.node1.y - self.node2.y)**2
         )
 
-        return length_squared ** 0.5        
+        return length_squared ** 0.5
 
-
-    def calculate_slope(self) -> Union[float]:
+    def calculate_slope(self) -> float:
         """
         Calculates and retuns slope of line connecting the two nodes
         """
@@ -45,27 +44,24 @@ class Arc:
         delta_y = y2 - y1
         delta_x = x2 - x1
 
-
         if delta_x == 0:
             return np.inf
 
-
         return -delta_y / delta_x
-
 
     def get_quadrant_of_node2(self) -> Literal[1, 2, 3, 4]:
         """
         Returns quadrant of node2 in reference to node1 based on the unit circle.
         Note: This takes into account the pygame cartesian coordinate system behavior.
         """
-        
+
         # TODO: check behavior of edge cases: x1 == x2 | y1 == y2
 
         x1, y1, x2, y2 = self.node1.x, self.node1.y, self.node2.x, self.node2.y
 
         # Left
         if x2 < x1:
-            
+
             # Top
             if y2 < y1:
                 return 2
@@ -82,12 +78,10 @@ class Arc:
                 return 1
 
             # Bottom or center
-            else: 
+            else:
                 return 4
 
-
-
-    def _adjust_for_quadrant(self, angle_in_radians: float) -> float:
+    def _adjust_angle_for_quadrant(self, angle_in_radians: float) -> float:
 
         quadrant = self.get_quadrant_of_node2()
 
@@ -103,22 +97,20 @@ class Arc:
         return angle_in_radians
 
     def calculate_angle(self, in_radians=False) -> float:
-
         """
         Note: angle is calculated according to positive x-axis
         """
 
         # Angle formula is the following:
-        #   
+        #
         #       Angle = arctan(slope)
 
         angle_in_radians = np.arctan(self.slope)
 
-        angle_in_radians = self._adjust_for_quadrant(angle_in_radians)
-
+        angle_in_radians = self._adjust_angle_for_quadrant(angle_in_radians)
 
         if in_radians:
-            
+
             return angle_in_radians
 
         else:
