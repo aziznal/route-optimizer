6a65166cb5fea0728ab5d107d2aa7e10f879d923
aziznal <aziznal.dev@gmail.com>
2021-05-21 18:56:37 +0300

    Formats Building.py and adds TODO

--- Building.py
+++ Building.py
@@ -1,26 +1,22 @@
+import pygame
 
 
-from typing import List
-
+from PygameSettings import *
 
 from BaseClasses.Node import Node
 
 
-import pygame
-
-from PygameSettings import *
-
+# TODO: Add method to draw node index or coordinates on top of node
 
 class Building(Node):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.rect = pygame.Rect(self.x, self.y, NODE_RADIUS, NODE_RADIUS)
 
-    def draw(self, screen: pygame.Surface):
+    def draw(self, screen: pygame.Surface) -> None:
 
         pygame.draw.ellipse(
             screen, Colors.RED, self.rect
         )
-
